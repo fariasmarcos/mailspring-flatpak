14adf40985b0cffe20f5e908390389ad6f50c9b6
Marcos Farias <20694062+fariasmarcos@users.noreply.github.com>
2021-09-04 21:19:19 -0300

    Update build args with --no-gpg-verify

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 flatpak run org.flatpak.Builder --force-clean --repo=repo build-dir com.foundry376.Mailspring.json &&
-flatpak remote-add --user --if-not-exists mailspring-test repo
+flatpak remote-add --user --if-not-exists --no-gpg-verify mailspring-test repo
