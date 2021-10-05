75fc100bcb134b8a2277bba50254ddc297f25a0b
Marcos Farias <20694062+fariasmarcos@users.noreply.github.com>
2021-10-05 19:26:29 -0300

    Add --or-update flag to the installation command
    There shouldn't be the need to run the update command every time the build is completed.

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flath
 flatpak install --user --noninteractive flathub org.flatpak.Builder &&
 flatpak run org.flatpak.Builder --user --install-deps-from=flathub --force-clean --repo=repo build com.foundry376.Mailspring.json &&
 flatpak remote-add --user --if-not-exists --no-gpg-verify --no-enumerate mailspring-test repo &&
-flatpak install --user --noninteractive mailspring-test com.foundry376.Mailspring &&
+flatpak install --user --or-update --noninteractive mailspring-test com.foundry376.Mailspring &&
 exit 0
